87703f15d480315f8c84438fd8844a4e7de67cbd
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-07-03 10:27:12 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<link rel="stylesheet" href="css/style.css">
+<?php include_once("lib/layouts/header.php"); ?>
+
+<div class="home">
+    <div class="home-content">
+        <div class="home-video-grid-item">
+            
+        </div>
+    </div>
+</div>
