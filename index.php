aaf863c58fb55c5002bae75b3aeaef27a8861d4f
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-07-03 21:03:43 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <link rel="stylesheet" href="css/style.css">
 <?php include_once("lib/layouts/header.php"); ?>
+<?php include_once("lib/layouts/nav.php"); ?>
 
+<?php include_once("lib/function/function.php"); ?>
 <div class="home">
     <div class="home-content">
-        <div class="home-video-grid-item">
-            
+        <div class="row">
+            <?php uploded_videos(); ?>
         </div>
     </div>
 </div>
+
