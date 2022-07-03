53643b6c69fd3c3116516e488da335a53949d9b6
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-07-03 23:28:30 +0530

    Add files via upload

---
+++ lib/routes/index_loged.php
@@ -0,0 +1,21 @@
+<?php 
+  include_once("../function/function.php");
+    //check the loginSession is not empty to enter to this page
+  if(empty($_SESSION['loginSession'])){
+      header('location:../views/login.php');
+  }
+?>
+
+<link rel="stylesheet" href="../../css/style.css">
+<?php include_once("../layouts/header.php"); ?>
+
+
+<?php include_once("../function/function.php"); ?>
+<div class="home">
+    <div class="home-content">
+        <div class="row">
+            <?php uploded_videos(); ?>
+        </div>
+    </div>
+</div>
+
