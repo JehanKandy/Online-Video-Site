fd325ed977395a7d639bd33e32d7c7d2e7960d84
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-07-03 23:17:48 +0530

    Update admin.php

--- lib/routes/admin.php
+++ lib/routes/admin.php
@@ -1,2 +1,15 @@
+<?php 
+  include_once("../function/function.php");
+    //check the loginSession is not empty to enter to this page
+  if(empty($_SESSION['loginSession'])){
+      header('location:../views/login.php');
+  }
+
+  include_once("../layouts/header.php");
+?>
+
+
 <?php include_once("../layouts/header.php"); ?>
-<?php include_once("../function/function.php"); ?>
+
+
+<a href="../views/logout.php">Logout</a>
