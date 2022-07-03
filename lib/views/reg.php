89c35b86eaaaac1f96edc441a4f56ab5a4204f40
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-07-03 21:24:56 +0530

    Update reg.php

--- lib/views/reg.php
+++ lib/views/reg.php
@@ -10,17 +10,17 @@
         <?php ?>
         <div class="reg-content-body">
             <form action="<?php echo($_SERVER['PHP_SELF']); ?>" method="POST">
-                <p>Username : </p>
-                <input type="text" name="username" id="username" placeholder="Username" class="login-input" required><br><br>
+                <span class="label">Username : </span>
+                <input type="text" name="username" id="username" placeholder="Username" class="login-input" required>
 
-                <p>Email Address : </p>
-                <input type="email" name="email" id="email" placeholder="Email" class="login-input" required><br><br>
+                <span class="label">Email Address : </span>
+                <input type="email" name="email" id="email" placeholder="Email" class="login-input" required>
 
-                <p>Password : </p>
-                <input type="password" name="password" id="password" placeholder="Password" class="login-input" required><br><br>
+                <span class="label">Password : </span>
+                <input type="password" name="password" id="password" placeholder="Password" class="login-input" required>
 
-                <p>Confirm Password : </p>
-                <input type="password" name="cpassword" id="cpassword" placeholder="Confirm Password" class="login-input" required><br><br>
+                <span class="label">Confirm Password : </span>
+                <input type="password" name="cpassword" id="cpassword" placeholder="Confirm Password" class="login-input" required>
 
                 <input type="submit" value="Register" name="Register" class="login-btn">&nbsp;&nbsp;&nbsp;<input type="reset" value="Clear" class="clr-btn">
             </form>
