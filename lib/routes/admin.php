b354157bb17cce305ebc1ba803e6444f61274d46
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-07-06 04:59:21 +0530

    Update admin.php

--- lib/routes/admin.php
+++ lib/routes/admin.php
@@ -1,12 +1,13 @@
 <?php 
-  include_once("../function/function.php");
+    include_once("../../function/function.php");
 
-  if(empty($_SESSION['loginSession'])){
-      header('location:../views/login.php');
-  }
+    if(empty($_SESSION['loginSession'])){
+        header("../../views/login.php");
+    }
 
-  include_once("../layouts/header.php");
-  include_once("../layouts/nav_loged.php");
+    include_once("../../layouts/header.php");
+    include_once("../../layouts/nav_loged_video.php");
+
 ?>
 <link rel="stylesheet" href="../../css/style.css">
 
@@ -30,14 +31,14 @@
     </section>
     <section class="admin-panel">
         <div class="container-fluid">
-          <h1 class="display-4">Welcome to Dashboard</h1>
+          <h1 class="display-4">All Free Users</h1>
           <hr>
 
           <div class="row">
             <div class="col-auto">
-              <div class="card bg-primary text-white">
+              <div class="card bg-success text-white">
                 <div class="card-body">
-                  <h4><i class='fas fa-users' style='font-size:40px'></i>&nbsp;Users&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h4>
+                  <h4><i class='fas fa-users' style='font-size:40px'></i>&nbsp;Active Users&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h4>
                   <hr style="background-color:white">
                   <h5>
                     <b><?php count_users(); ?></b>
@@ -46,72 +47,64 @@
               </div>
             </div>     
             <div class="col-auto">
-              <div class="card bg-success text-white">
+              <div class="card bg-danger text-white">
                 <div class="card-body">
-                  <h4><i class='fas fa-user-tie' style='font-size:40px'></i>&nbsp;Pro - Users&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h4>
+                  <h4><i class='fas fa-user-alt-slash' style='font-size:40px'></i>&nbsp;Deactive Users&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h4>
                   <hr style="background-color:white">
                   <h5>
                     <b><?php count_pro_users(); ?></b>
                   </h5>
                 </div>
               </div>
-            </div>   
-            <div class="col-auto">
-              <div class="card bg-warning text-white">
-                <div class="card-body">
-                  <h4><i class='fas fa-tv' style='font-size:40px'></i>&nbsp;Channels &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h4>
-                  <hr style="background-color:white">
-                  <h5>
-                    <b><?php count_channels(); ?></b>
-                  </h5>
-                </div>
-              </div>
-            </div>   
-            <div class="col-auto">
-              <div class="card bg-info text-white">
-                <div class="card-body">
-                  <h4><i class='far fa-file-video' style='font-size:40px'></i>&nbsp;Videos&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h4>
-                  <hr style="background-color:white">
-                  <h5>
-                    <b><?php count_videos(); ?></b>
-                  </h5>
-                </div>
-              </div>
-            </div>   
-            <div class="col-auto">
-              <div class="card bg-success text-white">
-                <div class="card-body">
-                  <h4><i class='fas fa-film' style='font-size:40px'></i>&nbsp;Pro - Videos&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h4>
-                  <hr style="background-color:white">
-                  <h5>
-                    <b><?php count_pro_videos(); ?></b>
-                  </h5>
-                </div>
-              </div>
-            </div>         
-            <div class="col-auto">
-              <div class="card bg-info text-white">
-                <div class="card-body">
-                  <h4><i class='fas fa-user-alt' style='font-size:40px'></i>&nbsp;Admins&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h4>
-                  <hr style="background-color:white">
-                  <h5>
-                    <b><?php count_admins(); ?></b>
-                  </h5>
-                </div>
-              </div>
-            </div>     
-            <div class="col-auto">
-              <div class="card bg-warning text-white">
-                <div class="card-body">
-                  <h4><i class='fas fa-lightbulb' style='font-size:40px'></i>&nbsp;Categories&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h4>
-                  <hr style="background-color:white">
-                  <h5>
-                    <b><?php count_catagery(); ?></b>
-                  </h5>
-                </div>
-              </div>
-            </div>   
-          </div>
-        </div>        
+            </div> 
+        </div>
+        
+        <br>
+        <hr>
+        <br>
+            <h2>Data in Table with pagination</h2>
+            <table class="table table-fluid" id="myTable">
+            <thead>
+            <tr>
+                <td>
+                Name
+                </td>
+                <td>
+                Class
+                </td>
+                <td>
+                Address
+                </td>
+                <td>
+                Mobile
+                </td>
+            </tr>
+            </thead>
+            <tbody>
+            <tr><td>Jehan</td><td>10</td><td>Kandy</td><td>+91 711758851</td></tr>
+            <tr><td>Jehan</td><td>10</td><td>Kandy</td><td>+91 711758851</td></tr>
+            <tr><td>Jehan</td><td>10</td><td>Kandy</td><td>+91 711758851</td></tr>
+            <tr><td>Jehan</td><td>10</td><td>Kandy</td><td>+91 711758851</td></tr>
+            <tr><td>Jehan</td><td>10</td><td>Kandy</td><td>+91 711758851</td></tr>
+            <tr><td>Jehan</td><td>10</td><td>Kandy</td><td>+91 711758851</td></tr>
+            <tr><td>Jehan</td><td>10</td><td>Kandy</td><td>+91 711758851</td></tr>
+            <tr><td>Jehan</td><td>10</td><td>Kandy</td><td>+91 711758851</td></tr>
+            <tr><td>Jehan</td><td>10</td><td>Kandy</td><td>+91 711758851</td></tr>
+            <tr><td>Jehan</td><td>10</td><td>Kandy</td><td>+91 711758851</td></tr>
+            <tr><td>Jehan</td><td>10</td><td>Kandy</td><td>+91 711758851</td></tr>
+            <tr><td>Jehan</td><td>10</td><td>Kandy</td><td>+91 711758851</td></tr>
+            <tr><td>Jehan</td><td>10</td><td>Kandy</td><td>+91 711758851</td></tr>
+            <tr><td>Jehan</td><td>10</td><td>Kandy</td><td>+91 711758851</td></tr>
+            <tr><td>Jehan</td><td>10</td><td>Kandy</td><td>+91 711758851</td></tr>
+            <tr><td>Jehan</td><td>10</td><td>Kandy</td><td>+91 711758851</td></tr>
+            <tr><td>Kamal</td><td>10</td><td>Kandy</td><td>+91 711758851</td></tr>
+            <tr><td>Jehan</td><td>10</td><td>Kandy</td><td>+91 711758851</td></tr>
+            <tr><td>Jehan</td><td>10</td><td>Kandy</td><td>+91 711758851</td></tr>
+            <tr><td>Jehan</td><td>10</td><td>Kandy</td><td>+91 711758851</td></tr>
+            </tbody>
+            </table>
+
     </section>
 </div>
+
+<script src="../../../script.js"></script>
