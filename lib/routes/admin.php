14c1187ea07d692b265e2141b55ce2a44fa56189
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-07-05 20:29:05 +0530

    Update admin.php

--- lib/routes/admin.php
+++ lib/routes/admin.php
@@ -13,13 +13,14 @@
 <div class="admin-content">
     <section class="sidebar">
         <ul class="nav-bar">
+            <li><a href="index_loged.php"><i class='far fa-file-video' style='font-size:20px'></i>&nbsp;&nbsp;View All Videos</a></li>
             <li><a href="#"><i class='fas fa-tachometer-alt' style='font-size:20px'></i>&nbsp;&nbsp;Dashboard</a></li>
             <li><a href="admin/all_user.php"><i class='fas fa-users' style='font-size:20px'></i>&nbsp;&nbsp;Users</a></li>
             <li><a href="#"><i class='fas fa-user-tie' style='font-size:20px'></i>&nbsp;&nbsp;Pro - Users</a></li>
             <li><a href="#"><i class='fas fa-tv' style='font-size:20px'></i>&nbsp;&nbsp;Channels</a></li>
             <li><a href="#"><i class='far fa-file-video' style='font-size:20px'></i>&nbsp;&nbsp;Videos</a></li>
             <li><a href="#"><i class='fas fa-film' style='font-size:20px'></i>&nbsp;&nbsp;Pro Videos</a></li>
-            <li><a href="#"><i class='fas fa-lightbulb' style='font-size:20px'></i>&nbsp;&nbsp;Admins</a></li>
+            <li><a href="#"><i class='fas fa-user-alt' style='font-size:20px'></i>&nbsp;&nbsp;Admins</a></li>
             <li><a href="#"><i class='fas fa-lightbulb' style='font-size:20px'></i>&nbsp;&nbsp;Categories</a></li>
             <li><a href="#"><i class='fas fa-cog' style='font-size:20px'></i>&nbsp;&nbsp;Account Settings</a></li>
 
@@ -32,17 +33,116 @@
           <hr>
 
           <div class="row">
-            <div class="col-md-3">
+            <div class="col-auto">
               <div class="card bg-primary text-white">
                 <div class="card-body">
-                  <h4><i class='fas fa-users' style='font-size:40px'></i>&nbsp;Users</h4>
+                  <h4><i class='fas fa-users' style='font-size:40px'></i>&nbsp;Users&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h4>
                   <hr style="background-color:white">
                   <h5>
                     <b><?php //count_users(); ?></b>
                   </h5>
                 </div>
               </div>
-            </div>            
+            </div>     
+            <div class="col-auto">
+              <div class="card bg-primary text-white">
+                <div class="card-body">
+                  <h4><i class='fas fa-users' style='font-size:40px'></i>&nbsp;Pro - Users&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h4>
+                  <hr style="background-color:white">
+                  <h5>
+                    <b><?php //count_users(); ?></b>
+                  </h5>
+                </div>
+              </div>
+            </div>   
+            <div class="col-auto">
+              <div class="card bg-primary text-white">
+                <div class="card-body">
+                  <h4><i class='fas fa-users' style='font-size:40px'></i>&nbsp;Channels &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h4>
+                  <hr style="background-color:white">
+                  <h5>
+                    <b><?php //count_users(); ?></b>
+                  </h5>
+                </div>
+              </div>
+            </div>   
+            <div class="col-auto">
+              <div class="card bg-primary text-white">
+                <div class="card-body">
+                  <h4><i class='fas fa-users' style='font-size:40px'></i>&nbsp;Videos&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h4>
+                  <hr style="background-color:white">
+                  <h5>
+                    <b><?php //count_users(); ?></b>
+                  </h5>
+                </div>
+              </div>
+            </div>   
+            <div class="col-auto">
+              <div class="card bg-primary text-white">
+                <div class="card-body">
+                  <h4><i class='fas fa-users' style='font-size:40px'></i>&nbsp;Pro - Videos&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h4>
+                  <hr style="background-color:white">
+                  <h5>
+                    <b><?php //count_users(); ?></b>
+                  </h5>
+                </div>
+              </div>
+            </div>         
+            <div class="col-auto">
+              <div class="card bg-primary text-white">
+                <div class="card-body">
+                  <h4><i class='fas fa-users' style='font-size:40px'></i>&nbsp;Admins&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h4>
+                  <hr style="background-color:white">
+                  <h5>
+                    <b><?php //count_users(); ?></b>
+                  </h5>
+                </div>
+              </div>
+            </div>     
+            <div class="col-auto">
+              <div class="card bg-primary text-white">
+                <div class="card-body">
+                  <h4><i class='fas fa-users' style='font-size:40px'></i>&nbsp;Categories&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h4>
+                  <hr style="background-color:white">
+                  <h5>
+                    <b><?php //count_users(); ?></b>
+                  </h5>
+                </div>
+              </div>
+            </div>   
+            <div class="col-auto">
+              <div class="card bg-primary text-white">
+                <div class="card-body">
+                  <h4><i class='fas fa-users' style='font-size:40px'></i>&nbsp;Channels &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h4>
+                  <hr style="background-color:white">
+                  <h5>
+                    <b><?php //count_users(); ?></b>
+                  </h5>
+                </div>
+              </div>
+            </div>   
+            <div class="col-auto">
+              <div class="card bg-primary text-white">
+                <div class="card-body">
+                  <h4><i class='fas fa-users' style='font-size:40px'></i>&nbsp;Videos&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h4>
+                  <hr style="background-color:white">
+                  <h5>
+                    <b><?php //count_users(); ?></b>
+                  </h5>
+                </div>
+              </div>
+            </div>   
+            <div class="col-auto">
+              <div class="card bg-primary text-white">
+                <div class="card-body">
+                  <h4><i class='fas fa-users' style='font-size:40px'></i>&nbsp;Pro - Videos&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h4>
+                  <hr style="background-color:white">
+                  <h5>
+                    <b><?php //count_users(); ?></b>
+                  </h5>
+                </div>
+              </div>
+            </div>         
           </div>
         </div>        
     </section>
