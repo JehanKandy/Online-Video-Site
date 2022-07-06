456be9847d239d1ae11b6254de5a49d8d39e0176
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-07-06 14:37:57 +0530

    Add files via upload

---
+++ lib/routes/admin/edit_pro_user_info.php
@@ -0,0 +1,28 @@
+<?php 
+    include_once("../../function/function.php");
+
+    if(empty($_SESSION['loginSession'])){
+        header("../../views/login.php");
+    }
+
+    include_once("../../layouts/header.php");
+    include_once("../../layouts/nav_loged_video.php");
+
+?>
+
+
+<?php 
+    if(isset($_POST['update'])){
+        $result = update_user_pro($_POST['username'], $_POST['user_status']);
+        echo $result;
+    }
+?>
+
+<div class="update-page">
+    <div class="update-content">
+        <div class="title">
+            Update Personal Information
+        </div>
+        <?php update_to_view_info(); ?>
+    </div>
+</div>
