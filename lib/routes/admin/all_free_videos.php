20f95c96cc575e0a63f3228516e81145387b6ce8
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-07-07 10:52:33 +0530

    Update all_free_videos.php

--- lib/routes/admin/all_free_videos.php
+++ lib/routes/admin/all_free_videos.php
@@ -19,8 +19,8 @@
             <li><a href="all_free_users.php"><i class='fas fa-users' style='font-size:20px'></i>&nbsp;&nbsp;Users</a></li>
             <li><a href="all_pro_users.php"><i class='fas fa-user-tie' style='font-size:20px'></i>&nbsp;&nbsp;Pro - Users</a></li>
             <li><a href="all_channels.php"><i class='fas fa-tv' style='font-size:20px'></i>&nbsp;&nbsp;Channels</a></li>
-            <li><a href="all_free_videos.php"><i class='far fa-file-video' style='font-size:20px'></i>&nbsp;&nbsp;Videos</a></li>
-            <li><a href="#"><i class='fas fa-film' style='font-size:20px'></i>&nbsp;&nbsp;Pro Videos</a></li>
+            <li><a href="#"><i class='far fa-file-video' style='font-size:20px'></i>&nbsp;&nbsp;Videos</a></li>
+            <li><a href="all_pro_videos.php"><i class='fas fa-film' style='font-size:20px'></i>&nbsp;&nbsp;Pro Videos</a></li>
             <li><a href="#"><i class='fas fa-user-alt' style='font-size:20px'></i>&nbsp;&nbsp;Admins</a></li>
             <li><a href="#"><i class='fas fa-lightbulb' style='font-size:20px'></i>&nbsp;&nbsp;Categories</a></li>
             <li><a href="#"><i class='fas fa-tv' style='font-size:20px'></i>&nbsp;&nbsp;My Channel</a></li>
@@ -38,7 +38,7 @@
             <div class="col-auto">
               <div class="card bg-success text-white">
                 <div class="card-body">
-                  <h4><i class='fas fa-user-tie' style='font-size:40px'></i>&nbsp;Active Videoss&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h4>
+                  <h4><i class='fas fa-user-tie' style='font-size:40px'></i>&nbsp;Active Videos&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h4>
                   <hr style="background-color:white">
                   <h5>
                     <b><?php count_videos(); ?></b>
@@ -62,8 +62,9 @@
         <br>
         <hr>
         <br>
-            <h2>All Free Users</h2>
-            <table class="table table-fluid" id="myTable">
+
+        <h2>All Free Users</h2>
+        <table class="table table-fluid" id="myTable">
             <thead>
             <tr>
                 <th>
