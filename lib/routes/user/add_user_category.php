6722ee5fbb7bcacac5c213bd2a9ca16e1fc7d3bb
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-07-15 12:36:13 +0530

    Add files via upload

---
+++ lib/routes/user/add_user_category.php
@@ -0,0 +1,43 @@
+<?php 
+    include_once("../../function/function.php");
+
+    if(empty($_SESSION['loginSession'])){
+        header("../../views/login.php");
+    }
+
+    include_once("../../layouts/header.php");
+    include_once("../../layouts/nav_loged_video.php");
+
+?>
+
+<div class="update-page">
+    <div class="update-content">
+        <div class="title">
+            Add Category
+        </div>
+        <div class="body">
+            <?php 
+                if(isset($_POST['add'])){
+                    $result = add_catogery_user($_POST['category_name'], $_POST['category_desc']);
+                    echo $result;
+                }            
+            ?>
+            <form action="<?php echo($_SERVER['PHP_SELF']); ?>" method="POST">
+                <table border="0">
+                    <tr>
+                        <td><span class="label">Category Name : </span></td>
+                        <td><input type="text" name="category_name" id="category_name" class="login-input"></td>
+                    </tr>
+                    <tr>
+                        <td><span class="label">Category Description : </span></td>
+                        <td><textarea name="category_desc" id="category_desc" class="text-area"></textarea></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2"><input type="submit" value="ADD Category" name="add" class="btn btn-success"></td>
+                    </tr>
+                </table>
+        
+            </form>
+        </div>
+    </div>
+</div>
