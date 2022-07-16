e6f7f21f1a3ceebd38d274a6be3a5f3b6cb34842
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-07-17 05:18:15 +0530

    Update otp_for_pwd.php

--- lib/routes/otp_for_pwd.php
+++ lib/routes/otp_for_pwd.php
@@ -9,3 +9,22 @@
 <link rel="stylesheet" href="../../css/style.css">
 <?php include_once("../layouts/header.php"); ?>
 
+<div class="pass-content">
+  <div class="pass-title">
+    Password Reset OTP
+  </div>
+  <?php 
+    if(isset($_POST['otp'])){
+      $result = check_otp($_POST['otp_num']);
+      echo $result;
+    }  
+  ?>
+  <div class="pass-body">
+    <form action="<?php echo($_SERVER['PHP_SELF']); ?>" method="post">
+      <span class="label">Enter OTP : </span>
+      <input type="number" name="otp_num" id="otp" placeholder="OTP" class="pass-input" required>
+      <br>
+      <input type="submit" value="Verify OTP" name="otp" class="btn btn-primary">
+    </form>
+  </div>
+</div>
