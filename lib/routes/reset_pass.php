415757a0f9a27bbd75b1e6f1627e73c529c7444f
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-07-17 06:00:35 +0530

    Add files via upload

---
+++ lib/routes/reset_pass.php
@@ -0,0 +1,36 @@
+<?php 
+  include_once("../function/function.php");
+    //check the loginSession is not empty to enter to this page
+  if(empty($_SESSION['loginSession'])){
+      header('location:../views/login.php');
+  }
+?>
+
+<link rel="stylesheet" href="../../css/style.css">
+<?php include_once("../layouts/header.php"); ?>
+
+<div class="pass-content">
+  <div class="pass-title">
+    Reset Password
+  </div>
+  <?php 
+    if(isset($_POST['reset'])){
+      $result = new_pass($_POST['otp_email'], $_POST['otp_password'], $_POST['otp_cpassword']);
+      echo $result;
+    }  
+  ?>
+  <div class="pass-body">
+    <form action="<?php echo($_SERVER['PHP_SELF']); ?>" method="post">
+      <span class="label">Enter Email : </span>
+      <input type="email" name="otp_email" id="otp_email" placeholder="Email" class="pass-input" required>
+      <br>
+      <span class="label">New Password : </span>
+      <input type="password" name="otp_password" id="otp_password" placeholder="New Password" class="pass-input" required>
+      <br>
+      <span class="label">Confirm New Password : </span>
+      <input type="password" name="otp_cpassword" id="otp_password" placeholder="Confirm New Password" class="pass-input" required>
+      <br>
+      <input type="submit" value="Reset Passowrd" name="reset" class="btn btn-primary">
+    </form>
+  </div>
+</div>
